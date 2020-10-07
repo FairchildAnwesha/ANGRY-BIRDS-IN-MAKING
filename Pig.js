49ee193974c7fa75735c150216aa3dd40455a679
FairchildAnwesha <71177721+FairchildAnwesha@users.noreply.github.com>
2020-10-07 16:57:09 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,13 @@
+class Pig extends BASSSSSSSSSSEEEEECLASSSSSSS {
+    constructor(x, y) {
+      
+      super(x, y, 50, 50);
+      
+
+      this.image= loadImage("sprites/enemy.png");
+
+      
+    }
+    
+  };
+  
